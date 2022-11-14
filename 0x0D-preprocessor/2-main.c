361c149533e6448214364a6e6f3c3fc21a5e0049
Ridwan Akinola <akinolaridwanbnhusaain@gmail.com>
2022-11-14 12:46:00 -0800

    fixed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,14 @@
+
+
 #include <stdio.h>
+/**
+ *main - prints the name of the file
+ *
+ *Return: always 0
+*/
 
-/* main: print the name of the file it compiled 
- * Return: always return 0
- * */
-
-int main(void);
+int main(void)
 {
 	printf("%s\n", __FILE__);
-	return(0);
+	return (0);
 }
